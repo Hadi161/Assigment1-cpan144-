4206ee9343f745316dac2434e4ef0bbb89d68873
Hadi161 <hadi70037@gmail.com>
2025-02-17 22:32:39 -0500

    Add files via upload

---
+++ Aboutpage.js
@@ -0,0 +1,15 @@
+import React from "react";
+// page to display information about this application
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page for my react app</h1>
+      <p>This is a page to display information about the app.</p>
+      <p> The main purpose of this app is to showcase diffrent features</p>
+    </div>
+  );
+}
+
+export default AboutPage;
+
+  
