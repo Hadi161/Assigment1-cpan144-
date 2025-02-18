5881fa73d17df564df56688e6ffea5c48acdeb7c
Hadi161 <hadi70037@gmail.com>
2025-02-17 22:29:13 -0500

    Add files via upload

--- page.js
+++ page.js
@@ -1,23 +1,12 @@
-//page.js for the components
-"use client";
 import React from "react";
-import Counter from "../components/Counter";
-import Toggle from "../components/Toggle";
-import Message from "../components/Message";
-
-const FeaturesPage = () => {
+// Main Page of the application
+const MainPage = () => {
   return (
     <div>
-      <h1>Features Page</h1>
-      <p>Welcome to the page where diffrent features are used.</p>
-      <Counter initalValue={4} />
-      <Toggle lablel="Light switch" />
-      <Message text="This is a message" color="red" />
+      <h1>Main Page</h1>
+      <p>Welcome to the Main Page!</p>
     </div>
   );
 }
 
-export default FeaturesPage;
-
-
-
+export default MainPage;
